81ee29ffc9e3be717690e00bc395fc9b2dd73de7
danijeljw-rpc <danijel@repasscloud.com>
2023-02-08 15:58:31 +1100

    update files

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 # rm -rf OptechX
 # git clone https://github.com/ShaileshMepa/OptechX.git
-rm -rf OptechX/__MACOSX
-rm -rf OptechX/preview
+# rm -rf OptechX/__MACOSX
+# rm -rf OptechX/preview
+
+DTAG="portal-fix"
+DVER="2.2b"
 
 docker build --rm --no-cache --tag "$DTAG:$DVER" .
